052867c97439ff458551fe374299b9d8ee7cac57
20mEmeraldSplash <yichenwaa@gmail.com>
2022-11-05 18:11:08 -0400

    Finish YIC-15

--- src/components/PageComponent/MainPage/MainPage.js
+++ src/components/PageComponent/MainPage/MainPage.js
@@ -1,6 +1,11 @@
 import React from "react";
 import logo from '../../../img/icon/sos_icon.png';
-import smile_icon from '../../../img/icon/HeaderBarIcons/smile.png';
+
+import chat_icon from '../../../img/icon/ButtonIcons/chat.png';
+import cloudy_icon from '../../../img/icon/ButtonIcons/cloudy.png';
+import payment_icon from '../../../img/icon/ButtonIcons/payment.png';
+import smile_icon from '../../../img/icon/ButtonIcons/smile.png';
+import web_icon from '../../../img/icon/ButtonIcons/web.png';
 
 import RotateImage from '../../RotateImage/RotateImage';
 import IconTextButton from "../../IconTextButton/IconTextButton";
@@ -9,18 +14,58 @@ import "./MainPage.css";
 export default function MainPage() {
   return (
     <div className="MainPage">
-      <header className="App-header">
-        <RotateImage 
-          imageSource = {logo}
-          imageWidth = "150px"
-          imageHeight = "150px"
-        />
-      </header>
-      <IconTextButton 
-          buttonIcon = {smile_icon}
-          buttonText = "WHO AM I?"
-          linkedPage = "/profile"
-      />
+        <div class="column-left">
+          <IconTextButton 
+            buttonIcon = {smile_icon}
+            buttonText = "WHO AM I?"
+            linkedPage = "/profile"
+          />
+          <div className="row-right">
+            <IconTextButton 
+              buttonIcon = {chat_icon}
+              buttonText = "我还没想好"
+              linkedPage = "/"
+            />
+          </div>
+          
+          <IconTextButton 
+            buttonIcon = {payment_icon}
+            buttonText = "要吃饭不磕碜"
+            linkedPage = "/"
+          />
+        </div>
+
+        <div class="column-center">
+          <RotateImage 
+            imageSource = {logo}
+            imageWidth = "150px"
+            imageHeight = "150px"
+          />
+        </div>
+
+        <div class="column-right">
+          <div className="row-right">
+            <IconTextButton 
+              buttonIcon = {web_icon}
+              buttonText = "没想好"
+              linkedPage = "/"
+            />
+          </div>
+          <IconTextButton 
+            buttonIcon = {cloudy_icon}
+            buttonText = "也没想好"
+            linkedPage = "/"
+          />
+          <div className="row-right">
+            <IconTextButton 
+              buttonIcon = {smile_icon}
+              buttonText = "我也没想好"
+              linkedPage = "/"
+            />
+          </div>
+        </div>
+      
+      
     </div>
   );
 }
