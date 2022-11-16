9df0fea939c601e4971d9e03415d49529f51e88d
20mEmeraldSplash <yichenwaa@gmail.com>
2022-11-16 14:47:01 -0500

    Finish YIC-19

--- src/components/PageComponent/ProfilePage/ProfilePage.js
+++ src/components/PageComponent/ProfilePage/ProfilePage.js
@@ -1,14 +1,27 @@
 import React from "react";
 import chesspattern from "../../../img/boarderImages/chess-pattern.png";
 import heart from "../../../img/boarderImages/heart.png";
+import happy_icon from '../../../img/icon/HeaderBarIcons/smile.png';
 
 import ResumeContainer from "../../ResumeContainer/ResumeContainer";
+import FieldsetContainer from "../../FieldsetContainer/FieldsetContainer";
 import './ProfilePage.css';
 
 export default function ProfilePage() {
+  const ResumeData = require('../../../data/resume.json');
+  console.log(ResumeData.Briefs[1].content);
   return (
     <div className="ProfilePage">
-      <ResumeContainer />
+      <div className="ResumeCol">
+        <ResumeContainer />
+        <FieldsetContainer 
+          legendTitle = {"BRIEF"}
+          contentData = {ResumeData.Briefs}
+          legendIcon = {happy_icon}
+        />
+        
+      </div>
+      
     </div>
   );
 }
