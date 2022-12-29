5b5b9bbbea3c48775948cd1ff9e782ef02b02fc9
20mEmeraldSplash <yichenwaa@gmail.com>
2022-12-29 11:21:19 -0500

    Finish YIC-46

---
+++ src/components/PageComponent/Magazine/MainPage/MagazineMainPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./MagazineMainPage.css";
+import Logo from "../../../../img/MainPage/logo.png";
+
+export default function MagazineMainPage(){
+    return(
+        <article className="MainPageContainer">
+            <section className="nameSection">
+                <Link to='/profile'>
+                    <div className="nameText">汪辰辰</div> 
+                </Link>
+                <div> &nbsp;/&nbsp; </div>
+                <Link to='/profile' >
+                    <div className="nameText">CHENCHEN WANG</div> 
+                </Link>
+            </section>
+            <section>
+                <img
+                    src={Logo}
+                    height = '70px'
+                    alt="birth-icon"
+                ></img>
+            </section>
+            
+        </article>
+    )
+}
